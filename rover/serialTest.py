c167c68e7904f5585685cb1cae3c33fd45eab91e
Luis Enrique <luis@artstudioh.com>
2018-11-24 13:04:26 -0800

    update

--- rover/serialTest.py
+++ rover/serialTest.py
@@ -1,19 +1,3 @@
-""" 
-This file will help you test the serial communication of the Raspberry Pi.
-To begin, use a jumper wire to connect the GPIO14 and GPIO15 pins.
-
-pinout: 
-https://goo.gl/images/K2HBYG
-
-python /home/pi/osr/rover/serialTest.py
-
-If the serial communication is working properly, this script will let you
-know that it successfully read data over the serial pins. If it does not, 
-verify that you have followed all the steps in this document to get serial
-communication working, and that you are connecting GPIO pins 14 and 15 
-together.
-"""
-
 import time
 import serial
 import threading
@@ -30,28 +14,28 @@ class Threads():
 			timeout=1
 			)
 
-	def SendData(self):
-		counter=0
-		while self.kill:
-			socket.write(str(counter))
-			time.sleep(1)
-			counter +=1
+def SendData(self):
+	counter=0
+	while self.kill:
+		socket.write(str(counter))
+		time.sleep(1)
+		counter +=1
 
-	def ReadData(self):
-		counter=0
-		while 1:
-			try:
-				x=self.ser.readline()
-				print 'Sucessful data recieved over serial!'
-				counter+=1
-			except:
-				print 'No serial data read, check wiring and software steps'
-				self.kill=0
-				break
-			if counter >= 3:
-				print "Serial communication verified! \nExiting"
-				self.kill=0
-				break
+def ReadData(self):
+	counter=0
+	while 1:
+		try:
+			x=self.ser.readline()
+			print 'Sucessful data recieved over serial!'
+			counter+=1
+		except:
+			print 'No serial data read, check wiring and software steps'
+			self.kill=0
+			break
+		if counter >= 3:
+			print "Serial communication verified! \nExiting"
+			self.kill=0
+			break
 
 def main():
 	mythread = Threads()
